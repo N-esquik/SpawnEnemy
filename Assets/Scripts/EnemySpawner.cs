3f09b34eeac435859f1ed85825f14f6fa286ec41
N-esquik <136918172+N-esquik@users.noreply.github.com>
2024-07-24 23:13:59 +0300

    Add files via upload

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private Transform[] _spawnPoint;
+    [SerializeField] private Vector3 _enemyPositionForward;
+    [SerializeField] private Vector3 _enemyPositionRight;
+
+    private float _spawnDelay = 2f;
+    private float _startSpawn = 0;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnEmeny), _startSpawn, _spawnDelay);
+    }
+
+    private void SpawnEmeny()
+    {
+        int randomPosition = Random.Range(0, _spawnPoint.Length);
+            
+        _enemy = Instantiate(_enemy, _spawnPoint[randomPosition].position, Quaternion.identity);
+        
+        if (randomPosition == 0 )
+        {
+            _enemy.SetDirectionMove(_enemyPositionRight);   
+        }
+        else
+        {
+            _enemy.SetDirectionMove(_enemyPositionForward);
+        }
+    }
+}
